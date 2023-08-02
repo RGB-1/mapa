94d0e311eeb489c6636379fd7c14bb465e593305
Ricky <rickygbarillas@gmail.com>
2023-08-02 11:07:13 -0600

    App Mapa

---
+++ src/components/loading.js
@@ -0,0 +1,11 @@
+import styles from './loading.module.css';
+
+export default function Loading () {
+    return (
+        <div className={styles.loadingContanier}>
+            <div className={styles.loader}>
+                <div></div>
+            </div>
+        </div>
+    );
+}
